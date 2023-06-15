1bdb7606a161fd43fe1e7b7ae14e006c56379cd2
novak_99 <cooljohn312@gmail.com>
2023-06-14 23:22:45 -0700

    particle

--- Particle/Particle.cpp
+++ Particle/Particle.cpp
@@ -1,1 +1,62 @@
-#include "Particle.hpp"
+#include "Particle/Particle.hpp"
+#include "Constants/Constants.hpp"
+
+namespace HyperEngine{
+
+    Particle::Particle(){ }
+
+    Particle::Particle(float mass){
+        this->mass = mass; 
+
+        this->force = zeroVector; 
+        this->velocity = zeroVector;
+        this->position = zeroVector;
+    }
+
+    Particle::Particle(Vector3 force, Vector3 velocity, Vector3 position, float mass){
+        this->force = force;
+        this->velocity = velocity;
+        this->position = position;
+    }
+
+    Vector3 Particle::getPosition(){
+        return position; 
+    }
+
+    void Particle::setPosition(Vector3 position){
+        this->position = position;
+    }
+
+    Vector3 Particle::getVelocity(){
+        return velocity;
+    }
+
+    void Particle::setVelocity(Vector3 position){
+        this->velocity = velocity;
+    }
+
+    Vector3 Particle::getAcceleration(){
+        return acceleration; 
+    }
+
+    void Particle::setAcceleration(Vector3 position){
+        this->acceleration = acceleration;
+    }
+
+    float Particle::getInverseMass(){
+        return inverseMass;
+    }
+
+    void Particle::setInverseMass(float inverseMass){
+        this->inverseMass = inverseMass; 
+    }
+
+    float Particle::getMass(){
+        return 1/inverseMass; 
+    }
+
+    void Particle::setMass(float mass){
+        this->inverseMass = 1/inverseMass; 
+    }
+
+}
--- Particle/Particle.hpp
+++ Particle/Particle.hpp
@@ -1,24 +1,40 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
-#include "Vector3.hpp"
+#include "Vector3/Vector3.hpp"
 
 namespace HyperEngine{
 
     class Particle{
-
         public:
-            Vector3 position; 
+            Obeject();
+            Particle(float mass);
+            Particle(Vector3 force, Vector3 velocity, Vector3 position, float mass);
 
+            Vector3 getPosition();
+            void setPosition(Vector3 position);
+
+            Vector3 getVelocity();
+            void setVelocity(Vector3 velocity);
+
+            Vector3 getAcceleration();
+            void setAcceleration(Vector3 force);
+
+            float getInverseMass();
+            void setInverseMass(float mass);
+
+            float getMass();
+            void setMass(float mass);
+
+        private:
+            Vector3 force; 
             Vector3 velocity; 
-
+            Vector3 position; 
             Vector3 acceleration; 
-
+            
             float damping; 
-
-        private: 
-
-
+            float inverseMass; 
     };
 }
+
 #endif // PARTICLE_HPP
